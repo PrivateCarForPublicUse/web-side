f7a9674da6a6b9bde2e2bf2ba047756718dc741a
郑培进 <1029445409@qq.com>
2019-11-28 22:50:27 +0800

    test

--- src/main/java/com/training/repository/UserTestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.training.repository;
-
-import com.training.domain.UserTest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTestRepository extends JpaRepository<UserTest,Long> {
-}
--- src/test/java/com/training/demo/DemoApplicationTests.java
+++ src/test/java/com/training/demo/DemoApplicationTests.java
@@ -1,13 +1,15 @@
 package com.training.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+        System.out.println("1234");
     }
 
 }
