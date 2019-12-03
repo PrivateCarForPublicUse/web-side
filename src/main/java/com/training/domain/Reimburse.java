141d9e0b48c9dc600d63167b609dcbfeb3fd24bb
heartcloud15 <415454927@qq.com>
2019-12-03 11:52:31 +0800

    字段名Routeid改为routeId

--- src/main/java/com/training/domain/Reimburse.java
+++ src/main/java/com/training/domain/Reimburse.java
@@ -10,14 +10,14 @@ public class Reimburse {
     @GeneratedValue
     private Long id;
     //报销id
-    private Long Routeid;
+    private Long routeId;
     //行程id
     private int isReimburse;
     //-1 报销失败；0 未报销；1 已报销
 
     public Reimburse(Long id, Long routeid, int isReimburse) {
         this.id = id;
-        Routeid = routeid;
+        routeId = routeid;
         this.isReimburse = isReimburse;
     }
 
@@ -30,11 +30,11 @@ public class Reimburse {
     }
 
     public Long getRouteid() {
-        return Routeid;
+        return routeId;
     }
 
     public void setRouteid(Long routeid) {
-        Routeid = routeid;
+        routeId = routeid;
     }
 
     public int getIsReimburse() {
