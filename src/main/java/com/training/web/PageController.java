0b174c6ecb5bbeb003dd2b519bb3e3f1fb0bf739
heartcloud15 <415454927@qq.com>
2019-12-03 18:15:50 +0800

    可以通过http://localhost:8080/user/index访问用户首页

--- src/main/java/com/training/web/PageController.java
+++ src/main/java/com/training/web/PageController.java
@@ -1,4 +1,17 @@
 package com.training.web;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Api(value="/",tags="页面跳转，测试用")
+@Controller
 public class PageController {
+    @ApiOperation("跳转到用户首页")
+    @GetMapping("/user/index")
+    public String userIndexPage(){
+        return "/user/front";
+    }
 }
