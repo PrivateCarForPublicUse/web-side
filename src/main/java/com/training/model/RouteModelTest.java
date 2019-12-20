3d4b5e45c96bab027140d9dd0232e7ffa243eddc
heartcloud15 <415454927@qq.com>
2019-12-20 16:22:43 +0800

    model例子

---
+++ src/main/java/com/training/model/RouteModelTest.java
@@ -0,0 +1,11 @@
+package com.training.model;
+
+import com.training.domain.Route;
+import com.training.domain.SecRoute;
+
+import java.util.List;
+
+public class RouteModelTest {
+    private Route route;
+    private List<SecRoute> secRoutes;
+}
