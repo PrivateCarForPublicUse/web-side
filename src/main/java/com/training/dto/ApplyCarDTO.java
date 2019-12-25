e45f5ed9da74bd3e3bfcce4ed78ccb0cd18bfc8a
zhanjiangxin <630673890@qq.com>
2019-12-25 13:46:31 +0800

    DTO增加两个接口所用到的实体类

---
+++ src/main/java/com/training/dto/ApplyCarDTO.java
@@ -0,0 +1,78 @@
+package com.training.dto;
+
+import java.util.List;
+
+public class ApplyCarDTO {
+    private String startTime;
+    private String endTime;
+    private String reason;
+    private List<String> names;
+    private List<PointLatDTO> lats;
+    private Long carId;
+    private Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public List<String> getNames() {
+        return names;
+    }
+
+    public void setNames(List<String> names) {
+        this.names = names;
+    }
+
+    public List<PointLatDTO> getLats() {
+        return lats;
+    }
+
+    public void setLats(List<PointLatDTO> lats) {
+        this.lats = lats;
+    }
+
+    public Long getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Long carId) {
+        this.carId = carId;
+    }
+
+    public ApplyCarDTO(String startTime, String endTime, String reason, List<String> names, List<PointLatDTO> lats, Long carId) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.reason = reason;
+        this.names = names;
+        this.lats = lats;
+        this.carId = carId;
+    }
+}
