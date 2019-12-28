aa031e3fb17396136a0ee6fd3d0391910432e2a9
郑培进 <1029445409@qq.com>
2019-12-28 18:22:55 +0800

    reimburse ok!

--- src/main/java/com/training/service/Impl/ReviewReimburseImpl.java
+++ src/main/java/com/training/service/Impl/ReviewReimburseImpl.java
@@ -3,6 +3,7 @@ package com.training.service.Impl;
 import com.training.domain.*;
 import com.training.dto.ReviewReimburseDTO;
 import com.training.repository.RouteRepository;
+import com.training.repository.SettlementRepository;
 import com.training.repository.UserRepository;
 import com.training.response.ResponseResult;
 import com.training.service.AccountService;
@@ -25,18 +26,28 @@ public class ReviewReimburseImpl implements ReviewReimburseService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    SettlementRepository settlementRepository;
     @Override
     public ResponseResult acceptReimburse(ReviewReimburseDTO reviewReimburseDTO, Account account) {
 
         if(account.getFlag()==1){
             Master master =(Master) accountService.getMasterByAccountId(account.getId()).getData();
             if(master.getCompanyId().equals(getCompanyId(reviewReimburseDTO))){
-                List<Settlement> list= routeRepository.findSettlementById(reviewReimburseDTO.getRouteId());
+                List<Settlement> list= settlementRepository.findSettlementByRouteId(reviewReimburseDTO.getRouteId());
                 Double sum=0.0;
                 for (Settlement settlement : list) {
+                    settlement.setDrivingCost(111);
+                    settlementRepository.save(settlement);
                     sum+=settlement.getDrivingCost();
                 }
-                return null;
+                Route route= routeRepository.findById(reviewReimburseDTO.getRouteId()).get();
+                route.setIsReimburse(1);
+                routeRepository.save(route);
+                User user = userRepository.findById(route.getUserId()).get();
+                user.setRemainMoney(user.getRemainMoney()+sum);
+                userRepository.save(user);
+                return new ResponseResult(200,"报销成功！",null);
             }
             return new ResponseResult(500,"别想报销到别的公司去！",null);
         }
