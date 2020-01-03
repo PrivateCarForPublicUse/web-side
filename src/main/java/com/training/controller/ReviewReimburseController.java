30dca0465d7fe9ca7574a0f97fe2958d0866e7db
heartcloud15 <415454927@qq.com>
2020-01-03 13:32:55 +0800

    增加拒绝报销的接口注释

--- src/main/java/com/training/controller/ReviewReimburseController.java
+++ src/main/java/com/training/controller/ReviewReimburseController.java
@@ -11,6 +11,8 @@ import com.training.service.ReimburseService;
 import com.training.service.ReviewReimburseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,10 +41,18 @@ public class ReviewReimburseController {
 
     @ApiOperation("拒绝报销")
     @PostMapping("/reject")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "成功"),
+            @ApiResponse(code = 500, message = "操作失败，管理员信息获取失败，请检查账户"),
+            @ApiResponse(code = 501, message = "相应路程不存在"),
+            @ApiResponse(code = 502, message = "message为空")
+    })
     public ResponseResult reject(@RequestBody ReviewReimburseDTO reviewReimburseDTO, HttpServletRequest request) {
+        if (reviewReimburseDTO.getMessage() == null || reviewReimburseDTO.getMessage().equals(""))
+            return new ResponseResult(502, "message为空");
         HttpSession session = request.getSession();
         Master master = (Master) session.getAttribute("master");
-        if(master==null)return new ResponseResult(500,"操作失败，管理员信息获取失败，请检查账户");
-        return reviewReimburseService.rejectReimburse(reviewReimburseDTO,master);
+        if (master == null) return new ResponseResult(500, "操作失败，管理员信息获取失败，请检查账户");
+        return reviewReimburseService.rejectReimburse(reviewReimburseDTO, master);
     }
 }
